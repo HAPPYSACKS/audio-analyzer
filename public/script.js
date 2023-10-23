90f5dfd20b8bd779e887002aa245dac50ab4e2c6
HAPPYSACKS <74272531+HAPPYSACKS@users.noreply.github.com>
2023-10-23 01:34:09 -0600

    updated repo from glitch

--- public/script.js
+++ public/script.js
@@ -18,6 +18,17 @@ let stream;
 let mediaRecorder;
 let audioChunks = [];
 
+function blobToBase64(blob) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onloadend = function () {
+      resolve(reader.result.split(",")[1]);
+    };
+    reader.onerror = reject;
+    reader.readAsDataURL(blob);
+  });
+}
+
 async function getSampleRateFromBlob(audioBlob) {
   return new Promise(async (resolve, reject) => {
     const audioContext = new (window.AudioContext ||
@@ -51,13 +62,16 @@ function downloadAudio(blob) {
 
 function initiateRecording(str, selectedPrompt) {
   mediaRecorder = new MediaRecorder(str, {
-    mimeType: "audio/webm;codecs=opus",
+    mimeType: "audio/mp4",
   });
   mediaRecorder.ondataavailable = (event) => {
     audioChunks.push(event.data);
   };
   mediaRecorder.onstop = async () => {
-    const audioBlob = new Blob(audioChunks, { type: "audio/webm;codecs=opus" });
+    document.getElementById("mic").style.display = "none"; // Hide the mic
+    document.getElementById("processingSpinner").style.display = "block"; // Show the spinner
+
+    const audioBlob = new Blob(audioChunks, { type: "audio/mp4" });
     audioChunks = [];
 
     try {
@@ -68,19 +82,22 @@ function initiateRecording(str, selectedPrompt) {
     }
 
     // downloadAudio(audioBlob);
+    console.log("How many times does this run?");
     const transcript = await sendToGCP(audioBlob);
-    document.getElementById("transcribedText").textContent = transcript;
+
     console.log(`transcript: ${transcript}`);
     const topic = selectedPrompt; // replace with the topic you're looking for
     const topicResponse = await checkTopicWithServer(transcript, topic);
     console.log(`Topic Response: ${topicResponse}`);
+
+    document.getElementById("mic").style.display = "block"; // Show the mic
+    document.getElementById("processingSpinner").style.display = "none"; // Hide the spinner
 
     str.getTracks().forEach((track) => track.stop());
     stream = null;
   };
 
   mediaRecorder.start();
-  document.getElementById("recordButton").textContent = "Stop Recording";
 }
 
 function stopRecording() {
@@ -97,15 +114,19 @@ function stopRecording() {
     tracks.forEach((track) => track.stop());
     stream = null;
   }
+  document.getElementById("mic").classList.remove("bobbing");
 }
 
 async function sendToGCP(blob) {
   const buffer = await blob.arrayBuffer();
+  console.log(`blob type: ${typeof buffer}`);
   const audioBytes = new Uint8Array(buffer);
-  const base64String = btoa(String.fromCharCode(...audioBytes));
+  const base64String = await blobToBase64(blob);
   const audio = {
     content: base64String,
   };
+
+  console.log("hi!");
 
   const request = {
     audio: audio,
@@ -151,12 +172,26 @@ async function checkTopicWithServer(transcript, topic) {
 
   const data = await response.json();
 
+  console.log(`gpt response ${data.topicResponse.message.content}`);
+
   if (data.topicResponse.message.content === "yes") {
     // Unlock the lock screen by changing the background to the home screen
     document.querySelector(".lock-screen").classList.add("unlocked");
-    document.getElementById("transcribedText").textContent = "Unlocked!";
+    document.querySelector(".time").style.display = "none";
+    document.querySelector(".date").style.display = "none";
+    document.getElementById("randomPrompt").style.display = "none";
+    document.getElementById("transcribedText").style.display = "none";
+    document.getElementById("mic").style.display = "none";
+    document.getElementById("processingSpinner").style.display = "none";
+    document.getElementById("lockIcon").style.display = "none";
   } else if (data.topicResponse.message.content === "no") {
-    document.getElementById("transcribedText").textContent = "Access Denied!";
+    // Assuming the lock icon has an id of 'lockIcon'
+    document.getElementById("lockIcon").classList.add("shake-icon");
+
+    // Optionally, remove the shake animation after it's done
+    setTimeout(() => {
+      document.getElementById("lockIcon").classList.remove("shake-icon");
+    }, 800); // The timeout should match the animation duration, which in this case is 0.8s or 800ms
   }
 
   return data.topicResponse;
@@ -170,36 +205,35 @@ window.onload = function () {
     "How do you feel about pineapples on pizza?",
     "Who is your role model and why?",
     "What book have you read recently?",
-    "Tell about a unique talent you have.",
+    "Talk about a unique talent you have.",
   ];
 
   const randomIndex = Math.floor(Math.random() * prompts.length);
   const selectedPrompt = prompts[randomIndex];
   document.getElementById("randomPrompt").textContent = selectedPrompt;
 
-  document
-    .getElementById("recordButton")
-    .addEventListener("click", function () {
-      if (
-        typeof mediaRecorder === "undefined" ||
-        mediaRecorder.state === "inactive"
-      ) {
-        if (!stream) {
-          navigator.mediaDevices
-            .getUserMedia({ audio: true })
-            .then((str) => {
-              stream = str;
-              initiateRecording(stream, selectedPrompt);
-            })
-            .catch((error) => {
-              console.error("Error accessing the microphone.", error);
-            });
-        } else {
-          initiateRecording(stream, selectedPrompt);
-        }
-      } else if (mediaRecorder.state === "recording") {
-        stopRecording();
-        this.textContent = "Start Recording";
+  document.getElementById("mic").addEventListener("click", function () {
+    if (
+      typeof mediaRecorder === "undefined" ||
+      mediaRecorder.state === "inactive"
+    ) {
+      if (!stream) {
+        navigator.mediaDevices
+          .getUserMedia({ audio: true })
+          .then((str) => {
+            stream = str;
+            initiateRecording(stream, selectedPrompt);
+          })
+          .catch((error) => {
+            console.error("Error accessing the microphone.", error);
+          });
+      } else {
+        initiateRecording(stream, selectedPrompt);
       }
-    });
+    } else if (mediaRecorder.state === "recording") {
+      stopRecording();
+      this.textContent = "Start Recording";
+    }
+    document.getElementById("mic").classList.add("bobbing");
+  });
 };
